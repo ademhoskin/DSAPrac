45790f4ff33a513964678b15f22abec4cdbe1f31
Adem Hoskin <99096322+ademhoskin@users.noreply.github.com>
2023-03-06 01:08:49 -0500

    on2 and non dominance

--- O(n).py
+++ O(n).py
@@ -1,6 +1,11 @@
 def print_items(n):
+    # o(n^2)
     for i in range(n):
         print(i)
-
+        for j in range(n):
+            print(j)
+    # + o(n) -- simplify to o(n^2) due to non dominance dropping
+    for k in range(n):
+        print(k)
 
 print_items(10)
